7b1f2777a4257a84bf66ec71cedc91c42c2e2654
Sanjay Choudhary <iamsanjaych@gmail.com>
2024-11-18 18:24:39 +0530

    update request

---
+++ src/app/(dashboard)/dashboard/requests/page.js
@@ -0,0 +1,40 @@
+import FriendRequests from '@/components/FriendRequests'
+import { authOptions } from '@/lib/auth'
+import { getServerSession } from 'next-auth'
+import { notFound } from 'next/navigation'
+import React from 'react'
+
+const page = async()=> {
+    const session = getServerSession(authOptions)
+    if(!session) notFound()
+        const incomingSenderIds = await fetchRedis(
+            'smembers',
+            `user:${session.user.id}:incoming_friend_requests`
+          );
+          
+          const incomingFriendRequests = await Promise.all(
+            incomingSenderIds.map(async (senderId) => {
+              const sender = await fetchRedis('get', `user:${senderId}`);
+              const senderParsed = JSON.parse(sender);
+          
+              return {
+                senderId,
+                senderEmail: senderParsed.email,
+              };
+            })
+          );
+          
+        return (
+            <main className='pt-8'>
+      <h1 className='font-bold text-5xl mb-8'>Add a friend</h1>
+      <div className='flex flex-col gap-4'>
+        <FriendRequests
+          incomingFriendRequests={incomingFriendRequests}
+          sessionId={session.user.id}
+        />
+      </div>
+    </main>
+  )
+}
+
+export default page
